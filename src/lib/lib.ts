da609928be50dabe0026e049af8e6521ae43b0e8
mateusz.swiderski <mateusz.swiderski@itm.com.pl>
2024-03-13 21:19:39 +0100

    Drag & Drop

--- src/lib/lib.ts
+++ src/lib/lib.ts
@@ -6,6 +6,7 @@ import type {VuelCalendarEvent} from "../utils/types/VuelCalendarEvent.ts";
 import type {VuelCalendarOptions} from "../utils/VuelCalendarOptions.ts";
 import type {Colors} from "../utils/types/Colors.ts";
 import type {IVuelCalendarOptions} from "../utils/VuelCalendarOptions.ts";
+import type {VuelCalendarDrop} from "../utils/types/VuelCalendarDrop.ts";
 import '../index.css';
-export type {IVuelCalendarApi,IVuelCalendarOptions, VuelCalendarDay, VuelCalendarEvent, VuelCalendarOptions, Colors}
+export type {IVuelCalendarApi,IVuelCalendarOptions, VuelCalendarDay, VuelCalendarEvent, VuelCalendarOptions, Colors, VuelCalendarDrop}
 export default VuelCalendar
